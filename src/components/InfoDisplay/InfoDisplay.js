4b7b9b9ef2e387b21bbdc3469e79841431316f9b
Luqman Ali <luqman.ali270304@outlook.com>
2024-11-16 22:07:51 +0000

    finished displaying information

--- src/components/InfoDisplay/InfoDisplay.js
+++ src/components/InfoDisplay/InfoDisplay.js
@@ -48,6 +48,33 @@ const InfoDisplay = ({ data, loading, error }) => {
                   </li>
                 ))}
             </ul>
+            <p className="text-left pt-10 text-gray-500">
+              <em>Synonyms</em>{' '}
+              <span className="text-customPurple font-semibold ml-4">
+                {data[0]?.meanings[0]?.synonyms || 'No synonyms'}
+              </span>
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col justify-start h-full w-full pt-10">
+        {data && (
+          <div className="flex items-center">
+            <p className="font-bold">{data[0]?.meanings[1]?.partOfSpeech}</p>
+            <div className="ml-4 w-full h-px bg-gray-500"></div>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col justify-start h-full w-fit pt-10">
+        {data && (
+          <div>
+            <p className="text-left">Meaning</p>
+            <ul className="list-disc text-sm md:lg:text-md lg:text-lg marker:text-customPurple text-left pl-5 ml-5 mt-2">
+              <li>{data[0]?.meanings[1]?.definitions[0]?.definition}</li>
+              <li className="list-none text-gray-400">
+                "{data[0]?.meanings[1]?.definitions[0]?.example || 'None'}"
+              </li>
+            </ul>
           </div>
         )}
       </div>
